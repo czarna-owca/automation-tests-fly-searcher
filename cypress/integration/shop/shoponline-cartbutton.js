4435bef0e8c77bab6233cf6f4ce9898620494f18
Czarna Owca <szydlowska_klaudia@protonmail.com>
2022-05-24 16:25:00 +0200

    cartbutton2

--- cypress/integration/shop/shoponline-cartbutton.js
+++ cypress/integration/shop/shoponline-cartbutton.js
@@ -3,17 +3,16 @@ describe('onlineshop-cartbutton', () => {
         cy.visit('http://automationpractice.com/')
 
     })
-    context('adding 1 item to cart', () => {
-        it('shows 1 item added', () => {
+
+    
+        it('shows pop up with sentence: There are 1 items in your cart. ', () => {
        
             let newItem = "t-shirt"
             cy.get('#search_query_top')
                 .type(`${newItem}{enter}`)  
             
             cy.get('.ajax_add_to_cart_button > span').click()
-            cy.get('btn btn-default button button-medium',{ timeout: 10000 }).click()
-            cy.get('ajax_cart_quantity').should('have.value','2')
+                        //cy.get('ajax_cart_quantity').should('have.value','2')
+            cy.get('.ajax_cart_product_txt ').should('.ajax_cart_product_txt ').to.contain('There are 1 items in your cart')
         })
-    })
-
 })
