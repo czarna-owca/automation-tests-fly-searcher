09f0958ec365adaf9a97bbd799a0725e14b0bde3
czarna-owca <szydlowska_klaudia@protonmail.com>
2022-06-28 19:07:13 +0200

    login test-success login

--- cypress/e2e/automationpractice-shoponline/shoponline-login.cy.js
+++ cypress/e2e/automationpractice-shoponline/shoponline-login.cy.js
@@ -4,9 +4,18 @@ describe('login test', () => {
         cy.visit('/')
     });
 
-    it('Success login', () => {
-        cy.visit('/')
+    it('Success login/logout', () => {
 
+        let newLogin = 'df@fmsd.com';
+        let newPassword = '123456';
+
+        cy.get('.login').click()
+        cy.get('#email').type(newLogin)
+        cy.get('#passwd').type(newPassword)
+        cy.get('#SubmitLogin > span').click();
+        cy.get('#center_column').should('contain', 'My account');
+        cy.get('.logout').click()
+        cy.get('.page-heading').should('contain', 'Authentication');
 
     });
 })
