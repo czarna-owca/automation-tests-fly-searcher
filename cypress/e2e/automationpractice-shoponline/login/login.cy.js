fe4217a1b73a0a12ca0a25ccd7b5e37c0a9602d1
czarna-owca <szydlowska_klaudia@protonmail.com>
2022-06-29 13:17:51 +0200

    homeShopPages-login.cy.js

--- cypress/e2e/automationpractice-shoponline/login/login.cy.js
+++ cypress/e2e/automationpractice-shoponline/login/login.cy.js
@@ -1,25 +1,33 @@
 import { Given, When, Then, And } from "cypress-cucumber-preprocessor/steps"
+const homeShopPage = require('../../e2e/homeShopPage')
+
 
 Given('open home page and click on sign in button', () => {
     cy.visit("/")
-    cy.get('.login').click()
+    //cy.get('.login').click()-- before homeshoppage
+
+    homeShopPage.click(signin)
 })
 
 When('Enter the Email address {string}', (username) => {
 
     //cy.get('#email').type(newLogin)
-    cy.get('#email').type(username)
+    // cy.get('#email').type(username)- before homeshoppage
+    homeShopPage.typeUsername(username)
 })
 
 And('Enter the password {string}', (password) => {
-    cy.get('#passwd').type(newPassword)
+    //cy.get('#passwd').type(password)- before homeshoppage
+
+    homeShopPage.typePassword(password)
 
 })
 
 
-And('Click on sign in button', () => {
+And('Click on green sign in button- loginbtn', () => {
 
-    cy.get('#SubmitLogin > span').click()
+    //  cy.get('#SubmitLogin > span').click()- before homeshoppage
+    homeShopPage.click(login)
 })
 
 Then('The user is successfully logged in', () => {
