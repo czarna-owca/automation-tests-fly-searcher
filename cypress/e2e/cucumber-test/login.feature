7b195619385fe89ee4c38e2cf36d2022759a50cd
czarna-owca <szydlowska_klaudia@protonmail.com>
2022-07-03 13:02:19 +0200

    2 scenario- success logout- login.feature

--- cypress/e2e/cucumber-test/login.feature
+++ cypress/e2e/cucumber-test/login.feature
@@ -4,14 +4,17 @@ Feature: Login Feature
 
   Scenario: 1. success login
     Given A user openes a home page and click on sign in button
-    When A user enter the username "<df@fmsd.com>"
-    And  A user enter the password "<123456>"
+    When A user enters the username "df@fmsd.com"
+    And A user enters the password "123456"
     And A user clicks on green sign in button- loginbtn
-    Then  A user will be successfully logged in
+    Then A user will be successfully logged in
+    Then A user will receive message "My account"
 
-  Scenario: 2. failed login - inwalid password message
+  Scenario: 2. success logout
     Given A user openes a home page and click on sign in button
-    When A user enters the username "<df@fmsd.com>"
-    And  A user enters the password "<123456>"
+    When A user enters the username "df@fmsd.com"
+    And A user enters the password "123456"
     And A user clicks on green sign in button- loginbtn
-    Then  A user will received message "<Invalid password>"
+    And A user clicks on sign out button
+    Then A user will be successfully logged out
+
--- cypress/e2e/cucumber-test/login/login.js
+++ cypress/e2e/cucumber-test/login/login.js
@@ -10,14 +10,14 @@ Given('A user openes a home page and click on sign in button', () => {
     //homeShopPage.click(signin)
 })
 
-When('A user enter the username {string}', (username) => {
+When('A user enters the username {string}', (username) => {
 
     //cy.get('#email').type(newLogin)
     cy.get('#email').type(username)//- before homeshoppage
     //homeShopPage.typeUsername(username)
 })
 
-And('A user enter the password {string}', (password) => {
+And('A user enters the password {string}', (password) => {
     cy.get('#passwd').type(password) // before homeshoppage
 
     //homeShopPage.typePassword(password)
@@ -33,12 +33,23 @@ And('A user clicks on green sign in button- loginbtn', () => {
 
 Then('A user will be successfully logged in', () => {
 
-    //cy.get('#center_column').should('contain', 'My account');
-    cy.url().should('contains', 'index.php?controller=my-account')
-
+    cy.url().should('contains', '/index.php?controller=my-account')
 })
 
 
-Then(' A user will received message {string}', (message) => {
-    cy.get('#center_column').should('have.text', message)
+Then('A user will receive message {string}', (message) => {
+
+    cy.get('#center_column').should('contain', message);
 })
+
+And('A user clicks on sign out button', () => {
+
+    cy.get('.logout').click()
+})
+
+Then('A user will be successfully logged out', () => {
+
+    cy.get('.page-heading').should('contain', 'Authentication')
+})
+
+
